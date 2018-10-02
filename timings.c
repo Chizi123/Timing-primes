c104d5df33eb2c3d2de839217246a7e00ded1c95
Chizi123 <joelgrun@gmail.com>
2018-10-02 19:27:15 +1000

    removed start and commented code

--- timings.c
+++ timings.c
@@ -6,24 +6,18 @@
 
 int main(void){
 	FILE * f1=fopen("times.csv","w");
-	int start=1,end;
-	// printf("Enter the number starting: ");
-	// scanf("%d",&start);
+	int end;
 	printf("Enter the number to end on: ");
 	scanf("%d",&end);
 	fprintf(f1,"num_test,start,end,time\n");
 
-	for(int i=start; i<=end; i++){
-		// long long start, end;
+	for(int i=1; i<=end; i++){
 		struct timespec s, e;
 		clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&s);
-		// s=GetTickCount();
 		prime(i);
-		// end=clock_gettime();
 		clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&e);
-		// fprintf(f1,"%d,%d,%d,%d\n",i,start,end,end-start);
 		fprintf(f1,"%d,%d,%d,%d\n",i,s.tv_nsec,e.tv_nsec,e.tv_nsec-s.tv_nsec);
-		printf("%d,%d,%d,%d\n",i,s.tv_nsec,e.tv_nsec,(e.tv_nsec-s.tv_nsec));
+		// printf("%d,%d,%d,%d\n",i,s.tv_nsec,e.tv_nsec,(e.tv_nsec-s.tv_nsec));
 		fflush(f1);
 		fflush(stdout);
 	}
